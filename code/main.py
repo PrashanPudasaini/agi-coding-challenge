eedc8d8bef8dda5633430c29043f912473eb24e3
Prashan Pudasaini <prashan.pudasaini@outlook.com>
2022-02-18 23:25:27 -0600

    Defined initial code structure

---
+++ code/main.py
@@ -0,0 +1,12 @@
+"""Return from cvs file - total gps message, total unique CAN messages, total CAN messages, total run time of data, 
+average CAN message per second and per gps message, first timestamp with most most CAN messages, first timestamp
+with least CAN messages.
+
+A module with "total_gps_message", "total_can_messages", " total_unique_can_messages", "total_run_time_of_data",
+"avg_can_msg", "most_can_msg", "least_can_message" and main function that processes csv file read into a pandas
+dataframe and prints the result to stdout.
+
+Author: Prashan Pudasaini <prashan.pudasaini@outlook.com>
+
+Created: February 18th, 2022
+"""
---
+++ modules/utils.py
@@ -0,0 +1,9 @@
+"""Run operations in parallel with multiprocessing.
+
+A module with "run_in_parallel" that takes all functions and runs them in parallel with python multiprocessing. 
+
+Author: Prashan Pudasaini <prashan.pudasaini@outlook.com>
+
+Created: February 18th, 2022
+"""
+
